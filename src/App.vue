22df53ec5a1b1a6dcf82139d0adb415595490c3b
xy <10816187@qq.com>
2024-05-10 11:37:19 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <el-button type="primary">element button</el-button>
+    <ElButton type="primary">element button</ElButton>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
